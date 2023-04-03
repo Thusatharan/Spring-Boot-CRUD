bd926fa3efff311a66659e96c80e44a4402af48e
Thusatharan <thusatharan27@gmail.com>
2023-04-03 07:41:25 +0530

    Added Customer Get Request

---
+++ src/main/java/Customer/CustomerController.java
@@ -0,0 +1,12 @@
+package Customer;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+    @GetMapping("customer")
+    Customer all() {
+        return new Customer("Thusanth", "thusanth@gmail.com");
+    }
+}
